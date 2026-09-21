84b5ba8dc0b441e55e830dd03469d74178429e2c
Esther C. de Sousa <esther.correa@aluno.educa.go.gov.br>
2026-09-21 18:01:09 +0000

    implementação objeto LIVRO e método adicionar

---
+++ LêAí/Data/LivroRepository.cs
@@ -0,0 +1,22 @@
+using LêAi_Models;
+
+namespace LêAi_Data
+{
+    public class LivroRepository
+    {
+        private static List<Livro> _tabelaLivros = new List<Livro>();
+
+        private static int proximoId = 1; // Variável auxiliar
+
+        public void Adicionar(Livro livro) //método: adiciona livros na lista
+        {
+
+            livro.Id = proximoId++;
+            _tabelaLivros.Add(livro);
+
+            
+        }
+
+
+    }
+}
